f4fb5742a5de9ccd200d45953f0a41b555a384fb
yang <695910779@qq.com>
2017-01-09 13:50:58 +0800

    测试合并呢分支

--- src/main/java/org/jasig/services/persondir/support/MultipleAttributeUserDao.java
+++ src/main/java/org/jasig/services/persondir/support/MultipleAttributeUserDao.java
@@ -13,7 +13,7 @@ public class MultipleAttributeUserDao extends StubPersonAttributeDao {
     @Override
     public IPersonAttributes getPerson(String uid) {
         Map<String, List<Object>> attributes = new HashMap<String, List<Object>>();
-        attributes.put("userid1", Collections.singletonList((Object) uid));
+        attributes.put("userid", Collections.singletonList((Object) uid));
         attributes.put("username", Collections.singletonList((Object) "测试username"));
         attributes.put("email", Collections.singletonList((Object) "test@163.com"));
         return new AttributeNamedPersonImpl(attributes);
